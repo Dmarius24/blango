c3aef1ed9eedb84568762285893f0874e4ac80b9
Marius Danciug <dmarius709@gmail.com>
2023-08-03 21:54:40 +0000

    Finish custom filters

---
+++ blog/templatetags/blog_extras.py
@@ -0,0 +1,37 @@
+from django import template
+from django.contrib.auth.models import User
+from django.utils.html import escape, format_html
+from django.utils.safestring import mark_safe
+
+
+
+register = template.Library()
+
+# Var 1 with easy mode
+# @register.filter
+# def author_details(author):
+#     return f"{author.first_name} {author.last_name}"
+
+# Var 2 the right choice
+@register.filter
+def author_details(author, current_user=None ):
+  if not isinstance(author, user_model):
+    # return empty string as safe default
+    return ""
+  
+  if author == current_user:
+    return format_html("<strong>me</strong>")
+
+  if author.first_name and author.last_name:
+    name = f"{author.first_name} {author.last_name}"
+  else:
+    name = f"{author.username}"
+
+  if author.email:
+    prefix = format_html('<a href="mailto:{}">', author.email)
+    suffix = format_html("</a>")
+  else:
+    prefix = ""
+    suffix = ""
+  
+  return format_html('{}{}{}', prefix, name, suffix)
